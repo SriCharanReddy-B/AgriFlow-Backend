9be6ceabc1ffd5ac8470c9bc070f6081c5c546e7
Sri_Charan <berellysricharan@gmail.com>
2026-02-14 16:46:14 +0530

    Day 13: Built React Frontend, Routing, and Global Cart State

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ app.use('/api/orders', require('./routes/orderRoutes')); // <--- ADD THIS
 // ... existing code
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));  
