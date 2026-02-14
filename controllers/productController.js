67bd46f403e18f8767fff1fca9056a1de3a4e527
Sri_Charan <berellysricharan@gmail.com>
2026-02-14 16:43:32 +0530

    "Day 13: Built React Frontend, Routing, and Global Cart State"

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,8 +31,22 @@ const createProduct = asyncHandler(async (req, res) => {
 
   res.status(201).json(product);
 });
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
 
 module.exports = {
   getProducts,
   createProduct,
+  getProductById, // <--- Add this line right here!
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, createProduct } = require('../controllers/productController');
+const { getProducts, createProduct, getProductById } = require('../controllers/productController');
 const { protect } = require('../middleware/authMiddleware'); // Import the Bouncer
 
 // Anyone can SEE products
@@ -9,4 +9,7 @@ router.get('/', getProducts);
 // Only Logged-in users can ADD products
 router.post('/', protect, createProduct);
 
+
+// ... existing routes
+router.get('/:id', getProductById); // <--- ADD THIS
 module.exports = router;
